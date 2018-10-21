4b12f5fa265c98f0ac7a5ac40324824759f030be
Lindsey Maddox <dev@lindseymaddox.com>
2018-10-21 09:08:04 -0500

    cant even get the examples to work. fml

--- main.js
+++ main.js
@@ -3,17 +3,8 @@
 // and change its text with the textContent property
 
 
-const sectionEl = document.querySelector(".article__section")
-
-/*
-    This code will only return the third section component
-*/
-const footerEl = document.querySelector(".article__footer")
-
-/*
-    Get reference to the section with the class of `smallText`
-    and remove that class. Then add a new class of `largeText`
-*/
-const smallTextEl = document.querySelector(".smallText")
-smallTextEl.classList.remove("smallText")
-smallTextEl.classList.add("largeText")
+// let poop = function(soop) {
+//   let nodeList = Array.from(document.querySelectorAll('.article__header'))
+//   nodeList.textContent("Welcome to the Lindsey Blog")
+//   console.log(poop)
+// }
